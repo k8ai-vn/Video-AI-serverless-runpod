28cdfa00c545e50569a90474d05afd646a580b85
Tu Nhat Quang <quangtu.tech@gmail.com>
2025-05-19 21:50:14 +0700

    Try LTX

--- rp_handler.py
+++ rp_handler.py
@@ -19,7 +19,7 @@ OUTPUT_PATH = os.path.join(NETWORK_STORAGE_PATH, 'outputs')
 # S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
 # S3_SECRET_KEY = os.environ.get('S3_SECRET_KEY')
 # # MODEL_NAME = 'Wan-AI/Wan2.1-T2V-1.3B-Diffusers'
-MODEL_NAME = 'Wan-AI/Wan2.1-T2V-1.3B-Diffusers'
+MODEL_NAME = 'Lightricks/LTX-Video'
 
 # # Create directories if they don't exist
 # os.makedirs(MODEL_CACHE_PATH, exist_ok=True)
@@ -142,7 +142,7 @@ def main():
         prompt=
         "Will Smith casually eats noodles, his relaxed demeanor contrasting with the energetic background of a bustling street food market. The scene captures a mix of humor and authenticity. Mid-shot framing, vibrant lighting.",
         seed=1024,
-        output_path="example_outputs/")
+        output_path=OUTPUT_PATH)
     
     generation_time = time.perf_counter() - gen_start_time
     print(f"Video generation time: {generation_time:.2f} seconds")
