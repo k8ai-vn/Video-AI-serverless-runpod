4a9ec262a88fa75fb25a0ca42731edfafaeac151
Tu Nhat Quang <quangtu.tech@gmail.com>
2025-05-19 21:31:13 +0700

    Fix

--- rp_handler.py
+++ rp_handler.py
@@ -1,5 +1,6 @@
-from fastvideo import VideoGenerator
-import os
+# import runpod
+# import time  
+# from fastvideo import VideoGenerator
 import os
 # import boto3
 # import datetime
@@ -20,23 +21,134 @@ OUTPUT_PATH = os.path.join(NETWORK_STORAGE_PATH, 'outputs')
 # # MODEL_NAME = 'Wan-AI/Wan2.1-T2V-1.3B-Diffusers'
 MODEL_NAME = 'Wan-AI/Wan2.1-T2V-1.3B-Diffusers'
 
+# # Create directories if they don't exist
+# os.makedirs(MODEL_CACHE_PATH, exist_ok=True)
+# os.makedirs(OUTPUT_PATH, exist_ok=True)
+
+# # Initialize S3 client
+# bucket_url = 'https://eu2.contabostorage.com/'
+# s3_client = boto3.client('s3',
+#                 endpoint_url=bucket_url,
+#                 aws_access_key_id=S3_ACCESS_KEY,
+#                 aws_secret_access_key=S3_SECRET_KEY,
+#                     config=Config(
+#                                 request_checksum_calculation="when_required",
+#                                 response_checksum_validation="when_required"
+#                             )
+#                 )
+
+# def upload_file(file_name, user_uuid, bucket, object_name=None):
+#     """Upload a file to an S3 bucket
+
+#     :param file_name: File to upload
+#     :param bucket: Bucket to upload to
+#     :param object_name: S3 object name. If not specified then file_name is used
+#     :return: True if file was uploaded, else False
+#     """
+#     # If S3 object_name was not specified, use file_name
+#     if object_name is None:
+#         object_name = os.path.basename(file_name)
+
+#     # Upload the file
+#     try:
+#         object_name = user_uuid + '/' + datetime.datetime.now().strftime('%d%m%Y%H%M%S') + '_' + file_name
+
+#         response = s3_client.upload_file(file_name, bucket, object_name)
+        
+#         # Create a presigned URL for the file
+#         presigned_url = s3_client.generate_presigned_url(
+#             'get_object',
+#             Params={'Bucket': bucket, 'Key': object_name},
+#             ExpiresIn=3600  # URL will be valid for 1 hour
+#         )   
+#         print(presigned_url)
+#         # delete file from local
+#         os.remove(file_name)
+        
+#     except ClientError as e:
+#         logging.error(e)
+#         return False
+#     return True
+
+
+
+# def handler(event):
+#     """
+#     This function processes incoming requests to your Serverless endpoint.
+    
+#     Args:
+#         event (dict): Contains the input data and request metadata
+        
+#     Returns:
+#         Any: The result to be returned to the client
+#     """
+    
+#     # Extract input data
+#     print(f"Worker Start")
+#     input = event['input']
+    
+#     prompt = input.get('prompt')  
+
+
+#     # Define a prompt for your video
+#     # prompt = "A curious raccoon peers through a vibrant field of yellow sunflowers, its eyes wide with interest."
+#     output_path = input.get('output_path', os.path.join(OUTPUT_PATH, 'my_videos/'))
+#     # Generate date time in DDMMYYYYHHMMSS format and random string
+#     current_time = datetime.datetime.now().strftime('%d%m%Y%H%M%S')
+#     random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+#     video_file_name = f"{current_time}_{random_string}.mp4"
+#     # Generate the video
+#     video = generator.generate_video(
+#         prompt,
+#         return_frames=True,  # Also return frames from this call (defaults to False)
+#         output_path=output_path,  # Controls where videos are saved
+#         save_video=True
+#     )
+#     os.rename(f"{output_path}{prompt}.mp4", f"{output_path}{video_file_name}")
+#     # Upload the video to S3
+#     upload_file(f"{output_path}{video_file_name}", input.get('user_uuid'), S3_BUCKET, video_file_name)
+#     return prompt
+
+# # Start the Serverless function when the script is run
+# if __name__ == '__main__':
+#     # Create a video generator with a pre-trained model
+#     generator = VideoGenerator.from_pretrained(
+#         "Wan-AI/Wan2.1-T2V-1.3B-Diffusers",
+#         num_gpus=2,  # Adjust based on your hardware
+#     )
+
+#     runpod.serverless.start({'handler': handler })
+
+import os
+import time
+
+from fastvideo import VideoGenerator
+
 def main():
-    # Create a video generator with a pre-trained model
-    generator = VideoGenerator.from_pretrained(
-        MODEL_NAME,
-        num_gpus=1,  # Adjust based on your hardware
+    # set the attention backend 
+    os.environ["FASTVIDEO_ATTENTION_BACKEND"] = "FLASH_ATTN"
+
+    start_time = time.perf_counter()
+    gen = VideoGenerator.from_pretrained(
+        model_path=MODEL_NAME,
+        num_gpus=2,
     )
+    load_time = time.perf_counter() - start_time
+    print(f"Model loading time: {load_time:.2f} seconds")
 
-    # Define a prompt for your video
-    prompt = "A curious raccoon peers through a vibrant field of yellow sunflowers, its eyes wide with interest."
+    gen_start_time = time.perf_counter()
 
-    # Generate the video
-    video = generator.generate_video(
-        prompt,
-        return_frames=True,  # Also return frames from this call (defaults to False)
-        output_path="my_videos/",  # Controls where videos are saved
-        save_video=True
-    )
+    gen.generate_video(
+        prompt=
+        "Will Smith casually eats noodles, his relaxed demeanor contrasting with the energetic background of a bustling street food market. The scene captures a mix of humor and authenticity. Mid-shot framing, vibrant lighting.",
+        seed=1024,
+        output_path="example_outputs/")
+    
+    generation_time = time.perf_counter() - gen_start_time
+    print(f"Video generation time: {generation_time:.2f} seconds")
 
-if __name__ == '__main__':
+    total_time = time.perf_counter() - start_time
+    print(f"Total execution time: {total_time:.2f} seconds")
+
+if __name__ == "__main__":
     main()
